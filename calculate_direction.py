f3dc32eb96c81588ed6d92de1e62067c8edb309c
Juan José Ramírez Gómez <jjramirezg09@gmail.com>
2023-04-14 12:02:57 -0500

    get snake and fruit wthout filter

---
+++ calculate_direction.py
@@ -0,0 +1,76 @@
+from queue import PriorityQueue
+import numpy as np
+def a_star(graph, start, goal):
+    # Creamos una cola de prioridad y agregamos el nodo inicial con una prioridad de 0
+    queue = PriorityQueue()
+    queue.put((0, start))
+    
+    # Diccionario que almacena los nodos visitados y su predecesor
+    visited = {start: None}
+    
+    # Diccionario que almacena los costos desde el inicio hasta cada nodo
+    g_score = {start: 0}
+    
+    # Loop principal del algoritmo
+    while not queue.empty():
+        # Extraemos el nodo con menor costo
+        current_cost, current_node = queue.get()
+        
+        # Si llegamos al nodo objetivo, construimos y devolvemos el camino
+        if current_node == goal:
+            path = []
+            while current_node is not None:
+                path.append(current_node)
+                current_node = visited[current_node]
+            return list(reversed(path))
+        
+        # Expandimos el nodo actual y revisamos sus vecinos
+        for neighbor, weight in graph[current_node].items():
+            # Si el vecino no es transitble, lo ignoramos
+            if weight == 0:
+                continue
+            
+            # Calculamos el costo desde el inicio hasta el vecino a través del nodo actual
+            tentative_g_score = g_score[current_node] + weight
+            
+            # Si el vecino ya ha sido visitado y el costo desde el inicio es menor que el calculado
+            # anteriormente, lo ignoramos
+            if neighbor in g_score and tentative_g_score >= g_score[neighbor]:
+                continue
+            
+            # Si el vecino no ha sido visitado o el costo desde el inicio es menor que el calculado
+            # anteriormente, lo agregamos a la cola de prioridad y actualizamos la información
+            visited[neighbor] = current_node
+            g_score[neighbor] = tentative_g_score
+            f_score = tentative_g_score + heuristic(neighbor, goal)
+            queue.put((f_score, neighbor))
+    
+    # Si no se encuentra un camino, se devuelve None
+    return None
+def heuristic(a, b):
+    return np.abs(a[0] - b[0]) + np.abs(a[1] - b[1])
+
+def move(previous_move, path, start):
+    print(path)
+    for node in path[1:]:
+    # Calcular la dirección en la que la serpiente debe moverse para seguir el camino
+        x, y = node
+        dx = x - start[0]
+        dy = y - start[1]
+        start = node
+        
+        # Enviar la tecla de flecha correspondiente para mover la serpiente en la dirección adecuada
+        if dx == 1 and previous_move!="left":
+            previous_move="right"
+        elif dx == -1 and previous_move!="right":
+            previous_move="left"
+        elif dy == 1 and previous_move!="up":
+            previous_move="down"
+        elif dy == -1 and  previous_move!="down":
+            previous_move="up"
+        print(previous_move)
+        print(node)
+        #pyautogui.press(previous_move)
+        #time.sleep(0.001)
+    print("crunch")
+    return(previous_move)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import io
 import base64
 import cv2
 import numpy as np
+from calculate_direction import move, a_star
 
 # Inicializar el navegador
 driver = webdriver.Chrome()
@@ -22,7 +23,7 @@ driver.implicitly_wait(100)
 play_button = driver.find_element(By.CLASS_NAME,"FL0z2d.Uxkl7b")
 play_button.click()
 canvas = driver.find_element(By.CLASS_NAME,"cer0Bd")
-
+previous_image = None
 # Acciones en el elemento canvas
 
 #actions = ActionChains(driver)
@@ -36,27 +37,21 @@ canvas = driver.find_element(By.CLASS_NAME,"cer0Bd")
 
 cell_size = 32
 def generate_graph():
-    width = 18*cell_size-4
-    height = 16*cell_size-7
-    canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
-    canvas_png = base64.b64decode(canvas_base64)
-    canvas_image = Image.open(io.BytesIO(canvas_png))
-    box = (29, 26, width, height)
-    ci = canvas_image.crop(box)
-    pixels = ci.load()
-    ci.show()
-
     # Define the dimensions of the board and the cell size
 
     # Initialize the graph
     graph = {}
     # Loop through the cells and add nodes to the graph
     for x in range(cell_size // 2, width-32, cell_size):
-        for y in range(cell_size // 2, height-32, cell_size-2):
+        for y in range(cell_size // 2, height-32, cell_size-1):
            r, g, b, a = pixels[x, y]
-           print(x//32,y//32, r,g,b)
-           if ((r < 215 or r > 240) and (g < 105 or g > 140)):
+           #print(x//32,y//32, r,g,b)
+           if ((r < 215 or r > 230) and (g < 105 or g > 140)):
             node = (x // cell_size, y // cell_size)
+            if (r == 255 and g == 255 and b == 255):
+                snake = node
+            elif (r == 231 and g == 71 and b == 29):
+                fruit = node
             graph[node] = {} 
                 # Check the neighboring cells to find connections
             for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
@@ -66,13 +61,15 @@ def generate_graph():
                     if ((r < 215 or r > 240) and (g < 105 or g > 140)):
                         neighbor = (nx // cell_size, ny // cell_size)
                         graph[node][neighbor] = 1
-    for node, neighbors in graph.items():
-        print(f"Node {node}:")
+    #for node, neighbors in graph.items():
+        #print(f"Node {node}:")
         #for neighbor, cost in neighbors.items():
            #print(f"  -> Neighbor {neighbor} (cost {cost})")#g = draw_graph_on_image(graph, ci)
-    g = draw_graph_on_image(graph,ci)
-    return graph
+    #g = draw_graph_on_image(graph,ci)
+    #print(snake)
+    return graph, snake, fruit
 
+'''
 def draw_graph_on_image(graph, image_path):
     # Load the original image
     image = image_path
@@ -88,9 +85,54 @@ def draw_graph_on_image(graph, image_path):
     cv2.imshow('Game Image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+'''
+while True:
+    width = 18*cell_size-4
+    height = 16*cell_size-7
+    canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
+    canvas_png = base64.b64decode(canvas_base64)
+    canvas_image = Image.open(io.BytesIO(canvas_png))
+    box = (28, 25, width, height)
+    ci = canvas_image.crop(box)
+    pixels = ci.load()
+    ci.show()
 
-graph=generate_graph()
+    current_image = np.array(ci)
+    if previous_image is not None:
+        previous_image = np.array(previous_image)
+
+        difference = np.sum(np.abs(current_image - previous_image))
+        
+        if difference != 0:
+            print("El canvas ha cambiado")
+
+        # Guardar la imagen actual para la próxima comparación
+        previous_image = ci
+
+        graph, snake, fruit = generate_graph()
+        print ("snake: ", snake)
+        print ("fruit: ", fruit)
+        if fruit is not None:
+                #direction = a_star(graph, snake_position, food_position)
+                    try:
+                    #direction = calculate_direction(direction, snake_position, food_position)
+                        astar=a_star(graph, snake, fruit)
+                    #print(astar)
+                        previous_move = move(previous_move, astar, snake)
+                    except:
+                        continue
+                #pyautogui.press(direction)
+                #pyautogui.press("space")
+
+                #print("Posición de la comida: ", food_position)
+            #print("Posición de la snake: ", snake_position)
+        else:
+            print("No se encontró comida en la imagen.")
+        # Emitir una orden de movimiento a la serpiente para que se mueva en la dirección adecuada
+    previous_image = ci
+
 # Dibujar la cuadrícula en el canvas
+    
 driver.execute_script()
 
 # Esperar un momento para ver la cuadrícula
