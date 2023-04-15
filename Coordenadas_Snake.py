f6125ca024492d96eb93d107c5e2400aaffb4e43
Judirale13 <93725600+Judirale13@users.noreply.github.com>
2023-04-15 12:53:39 -0500

    Identificar Craneo Snake

--- Coordenadas_Snake.py
+++ Coordenadas_Snake.py
@@ -18,13 +18,17 @@ cell_size=32
 def snake_position(ci):
     # Convertir la imagen a formato HSV
     hsv_image = cv2.cvtColor(np.array(ci), cv2.COLOR_RGB2HSV)
-    lower_orange = np.array([0, 0, 200])
-    upper_orange = np.array([255, 20, 255])
+    lower_orange = np.array([0, 0, 240])
+    upper_orange = np.array([255, 255, 255])
     # Crear una máscara que detecte los colores dentro de los límites especificados
     mask = cv2.inRange(hsv_image, lower_orange, upper_orange)
+    pil_image = Image.fromarray(mask)
+    pil_image = pil_image.filter(ImageFilter.GaussianBlur(radius=2))
+    cv2_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+    #pil_image.show()
     # Detectar los bordes en la imagen usando el operador Canny
-    blur_image = cv2.GaussianBlur(mask, (5, 5), 0)
-    canny_image = cv2.Canny(blur_image, 100, 200) 
+    #blur_image = cv2.GaussianBlur(cv2_image, (5, 5), 0)
+    canny_image = cv2.Canny(cv2_image, 100, 200) 
     contours, _ = cv2.findContours(canny_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
         largest_contour=max(contours, key=cv2.contourArea)
@@ -32,11 +36,11 @@ def snake_position(ci):
         if moments['m00'] > 0:
             cx = int(moments['m10'] / moments['m00'])
             cy = int(moments['m01'] / moments['m00'])
-        cv2.circle(mask, (cx, cy), 5, (255, 255, 255),-1)
-        cv2.circle(mask, (cx, cy), 2, (255, 255, 255))
-        cv2.imshow('Game Image', mask)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.circle(mask, (cx, cy), 5, (255, 255, 255),-1)
+        #cv2.circle(mask, (cx, cy), 2, (255, 255, 255))
+        #cv2.imshow('Game Image', cv2_image)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
         snake_head_position = (cx // cell_size, cy // cell_size)
         return snake_head_position
     
